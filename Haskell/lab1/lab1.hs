0af504fa7286466f45c56f1774777b449a55e8a4
Hammarkvast <tomham-3@student.ltu.se>
2020-04-13 22:39:21 +0200

    Updated outputting datastructure, only sorting and printing left

--- Haskell/lab1/lab1.hs
+++ Haskell/lab1/lab1.hs
@@ -1,11 +1,34 @@
-getSublist :: [Int] -> Int -> [[Int]]
-getSublist x y = [take y x]
 
-getSublists :: [Int] -> Int -> [[Int]]
-getSublists x 0 = []
-getSublists x y = getSublist x y ++ getSublists x (y-1)
 
-getAllSublists :: [Int] -> [[Int]]
-getAllSublists [] = []
-getAllSublists (x:xs)  = getSublists (x:xs) (length (x:xs)) ++ getAllSublists xs  
+getSum :: [Int] -> Int
+getSum [] = 0
+getSum (x:xs) = x + getSum xs
 
+sumOfSublists :: [[Int]] -> [Int]
+sumOfSublists x = [ getSum n | n <- x]
+
+qSort :: [Int] -> [Int]
+qSort [] = []
+qSort (x:xs) = qSort [a | a <- xs, a<=x] ++ [x] ++ qSort [n | n <- xs, n>x]
+
+sortMatrix :: [[Int]] -> [[Int]]
+sortMatrix [] = []
+sortMatrix (x:xs) = sortMatrix [less | less <- xs, sum less <= sum x] ++ [x] ++ sortMatrix [greater | greater <- xs, sum greater > sum x]
+
+-- getAllSublistsNew :: [Int] -> [(Int, Int, [Int])]
+-- getAllSublistsNew [] = []
+-- getAllSublistsNew (x:xs) = [(length (x:xs) - length xs,  length xs, getSublists (x:xs))] ++ getAllSublistsNew xs 
+
+
+
+getSublistsNew :: [Int] -> Int -> [(Int, Int, [Int])]
+getSublistsNew [] _ = []
+getSublistsNew x y= [(y,(y + length x) - 1, x)] ++ getSublistsNew (init x) y  
+
+getAllSublistsNew :: [Int] -> Int ->[(Int, Int, [Int])]
+getAllSublistsNew [] _= []
+getAllSublistsNew (x:xs) y = getSublistsNew (x:xs) y ++ getAllSublistsNew (tail (x:xs)) (y+1)
+
+-- generateSublists :: [Int] -> Int -> [(Int, Int [Int])]
+-- generateSublists [] _= []
+-- generateSublists [] = getAllSublistsNew 
