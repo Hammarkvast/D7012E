12623715082e2d5db0653c7aa705533ee3536ff8
Hammarkvast <tomham-3@student.ltu.se>
2020-04-14 22:04:08 +0200

    lab 1 probably done

--- Haskell/lab1/lab1.hs
+++ Haskell/lab1/lab1.hs
@@ -4,31 +4,50 @@ getSum :: [Int] -> Int
 getSum [] = 0
 getSum (x:xs) = x + getSum xs
 
-sumOfSublists :: [[Int]] -> [Int]
-sumOfSublists x = [ getSum n | n <- x]
+returnSumFromTuple :: (Int, Int, Int, [Int]) -> Int
+returnSumFromTuple (theSum, i, j, lst) = theSum
 
-qSort :: [Int] -> [Int]
-qSort [] = []
-qSort (x:xs) = qSort [a | a <- xs, a<=x] ++ [x] ++ qSort [n | n <- xs, n>x]
-
-sortMatrix :: [[Int]] -> [[Int]]
-sortMatrix [] = []
-sortMatrix (x:xs) = sortMatrix [less | less <- xs, sum less <= sum x] ++ [x] ++ sortMatrix [greater | greater <- xs, sum greater > sum x]
-
--- getAllSublistsNew :: [Int] -> [(Int, Int, [Int])]
--- getAllSublistsNew [] = []
--- getAllSublistsNew (x:xs) = [(length (x:xs) - length xs,  length xs, getSublists (x:xs))] ++ getAllSublistsNew xs 
+calcSum :: [(Int, Int, Int, [Int])] -> Int
+calcSum [(_, _, _, [])] = 0
+calcSum ((theSum,i,j,((start:end))):xs)  = start + calcSum ((theSum,i,j,((end))):xs) + calcSum xs 
 
 
+quickSort :: [(Int, Int, Int, [Int])] -> [(Int, Int, Int, [Int])]
+quickSort [] = []
+quickSort ((sumofLst, i, j, lst):xs) = quickSort [small | small <- xs, returnSumFromTuple small <= sumofLst] ++ 
+        [(sumofLst, i, j, lst)] ++ 
+        quickSort [larger | larger <- xs, returnSumFromTuple larger > sumofLst]
 
-getSublistsNew :: [Int] -> Int -> [(Int, Int, [Int])]
-getSublistsNew [] _ = []
-getSublistsNew x y= [(y,(y + length x) - 1, x)] ++ getSublistsNew (init x) y  
 
-getAllSublistsNew :: [Int] -> Int ->[(Int, Int, [Int])]
-getAllSublistsNew [] _= []
-getAllSublistsNew (x:xs) y = getSublistsNew (x:xs) y ++ getAllSublistsNew (tail (x:xs)) (y+1)
+getSublists :: [Int] -> Int -> [(Int, Int, Int, [Int])]
+getSublists [] _ = []
+getSublists x start = [(getSum x, start,(start + length x) - 1, x)] ++ getSublists (init x) start  
+
+getAllSublists :: [Int] -> Int ->[(Int, Int, Int, [Int])]
+getAllSublists [] _= []
+getAllSublists (x:xs) start = getSublists (x:xs) start ++ getAllSublists (tail (x:xs)) (start+1)
+
+generateString :: [(Int, Int, Int, [Int])] -> String
+generateString ((lstSum, i, j, lst):xs) 
+    |  xs == [] = ""
+    | otherwise = show lstSum ++ "\t" ++ show i ++ "\t" ++ show j ++ "\t" ++ show lst ++ "\n" ++ generateString xs
+
+generateKLst :: [Int] -> Int -> [(Int, Int, Int, [Int])]
+generateKLst [] _ = []
+generateKlst completeList k = let allSubLsts = getAllSublists completeList 0
+                                  sorted = quickSort allSubLsts
+                                in take k sorted
+
+smallestKset:: [Int] -> Int -> IO()
+--smallestKset [] _ =  
+smallestKset lst k = if length lst == 15 then putStrLn "End of List!" else 
+    let subLists = generateKlst lst k     
+        str = generateString subLists
+        in putStrLn (str)
+
+
+
 
 -- generateSublists :: [Int] -> Int -> [(Int, Int [Int])]
 -- generateSublists [] _= []
--- generateSublists [] = getAllSublistsNew 
+-- generateSublists [] = getAllSublists 
