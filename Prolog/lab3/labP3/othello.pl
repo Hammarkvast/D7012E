667a9a14904a5e5025cbeaf2e54dc6eaf1ff1045
Hammarkvast <tomham-3@student.ltu.se>
2020-05-26 15:32:20 +0200

    Implemented Moves

--- Prolog/lab3/labP3/othello.pl
+++ Prolog/lab3/labP3/othello.pl
@@ -66,7 +66,12 @@
 %
 % given helper: Inital state of the board
 
-initBoard([[.,.,.,.,.,.],[.,.,.,.,.,.],[.,.,1,2,.,.],[.,.,2,1,.,.],[.,.,.,.,.,.],[.,.,.,.,.,.]]).
+initBoard([[.,.,.,.,.,.],
+		   [.,.,.,.,.,.],
+		   [.,.,1,2,.,.],
+		   [.,.,2,1,.,.],
+		   [.,.,.,.,.,.],
+		   [.,.,.,.,.,.]]).
 
 % DO NOT CHANGE THIS BLOCK OF COMMENTS.
 %
@@ -95,7 +100,7 @@ score(Board, Player, Score) :-
 	flatten(Board, ScoreList),
 	countScore(ScoreList, Player, Score).
 
-countScore([], S, 0).
+countScore([], _, 0).
 countScore([Player | RestOfBoard], Player, TotalScore) :-
 	countScore(RestOfBoard, Player, Score),
 	TotalScore is 1 + Score.
@@ -103,6 +108,7 @@ countScore([Player | RestOfBoard], Player, TotalScore) :-
 countScore([NotPlayerStone | RestOfBoard], Player, Score) :-
 	Player \= NotPlayerStone,
 	countScore(RestOfBoard, Player, Score).
+
 
 winner(State, Plyr) :-
 	terminal(State),
@@ -169,7 +175,253 @@ printList([H | L]) :-
 
 
 
+getPlayerPositions(_, [], _, _, []).
+getPlayerPositions(Plyr, [Head | RestOfRow], X, Y, PlayerPositions) :-
+	Plyr = Head,
+	PlayerCoords = [[X, Y]],
+	NextX is X + 1,
+	getPlayerPositions(Plyr, RestOfRow, NextX, Y, NewCoords),
+	append(PlayerCoords, NewCoords, PlayerPositions).
+getPlayerPositions(Plyr, [Head | RestOfRow], X, Y, PlayerPositions) :-
+	Plyr \= Head,
+	NextX is X + 1,
+	getPlayerPositions(Plyr, RestOfRow, NextX, Y, PlayerPositions).
 
+getAllPlayerPositions(_, [], _, []).
+getAllPlayerPositions(Plyr, [FirstRow | RestOfRows], Y, Positions):-
+	getPlayerPositions(Plyr, FirstRow, 0, Y, PlayerRowPositions),
+	NextRow is Y + 1, 
+	getAllPlayerPositions(Plyr, RestOfRows, NextRow, RestRowPositions),
+	append(PlayerRowPositions, RestRowPositions, Positions).
+
+decreaseX(X, XMinusOne) :-
+	XMinusOne is X - 1.
+
+increaseX(X, XPlusOne) :-
+	XPlusOne is X + 1.
+
+decreaseY(Y, YMinusOne) :-
+	YMinusOne is Y - 1.
+
+increaseY(Y, YPlusOne) :-
+	YPlusOne is Y + 1.
+
+increaseXY(X, Y, XPlusOne, YPlusOne):-
+	XPlusOne is X + 1,
+	YPlusOne is Y + 1.
+
+decreaseXY(X, Y, XMinusOne, YMinusOne) :-
+	XMinusOne is X - 1,
+	YMinusOne is Y - 1.
+
+incXDecY(X, Y, XPlusOne, YMinusOne):-
+	XPlusOne is X + 1,
+	YMinusOne is Y - 1.
+
+decXIncY(X, Y, XMinusOne, YPlusOne) :-
+	XMinusOne is X - 1,
+	YPlusOne is Y + 1.
+
+countEnemiesWest(Plyr, [X,Y], State, ValidDot):-
+	get(State, [X,Y], Enemy),
+	Enemy \= Plyr,
+	Enemy \= .,
+	decreaseX(X, NewX),
+	countEnemiesWest(Plyr, [NewX, Y], State, ValidDot).
+countEnemiesWest(_, [X,Y], _, ValidDot):-
+	ValidDot = [[X, Y]].
+
+countEnemiesEast(Plyr, [X,Y], State, ValidDot) :-
+	get(State, [X,Y], Enemy),
+	Enemy \= Plyr,
+	Enemy \= .,
+	increaseX(X, NewX),
+	countEnemiesEast(Plyr, [NewX, Y], State, ValidDot).
+countEnemiesEast(_, [X,Y], _, ValidDot):-
+	ValidDot = [[X, Y]].
+
+countEnemiesNorth(Plyr, [X,Y], State, ValidDot):-
+	get(State, [X,Y], Enemy),
+	Enemy \= Plyr,
+	Enemy \= .,
+	decreaseY(Y, NewY),
+	countEnemiesNorth(Plyr, [X, NewY], State, ValidDot).
+countEnemiesNorth(_, [X,Y], _, ValidDot):-
+	ValidDot = [[X, Y]].
+
+countEnemiesSouth(Plyr, [X, Y], State, ValidDot) :-
+	get(State, [X,Y], Enemy),
+	Enemy \= Plyr,
+	Enemy \= .,
+	increaseY(Y, NewY),
+	countEnemiesNorth(Plyr, [X, NewY], State, ValidDot).
+countEnemiesSouth(_, [X,Y], _, ValidDot):-
+	ValidDot = [[X, Y]].
+
+countEnemiesSE(Plyr, [X, Y], State, ValidDot) :-
+	get(State, [X,Y], Enemy),
+	Enemy \= Plyr,
+	Enemy \= .,
+	increaseXY(X,Y, NewX, NewY),
+	countEnemiesSE(Plyr, [NewX, NewY], State, ValidDot).
+countEnemiesSE(_, [X,Y], _, ValidDot):-
+	ValidDot = [[X, Y]].
+
+countEnemiesSW(Plyr, [X,Y], State, ValidDot) :-
+	get(State, [X,Y], Enemy),
+	Enemy \= Plyr,
+	Enemy \= .,
+	decreaseXY(X,Y, NewX, NewY),
+	countEnemiesSW(Plyr, [NewX, NewY], State, ValidDot).
+countEnemiesSW(_, [X,Y], _, ValidDot):-
+	ValidDot = [[X, Y]].
+
+countEnemiesNE(Plyr, [X,Y], State, ValidDot) :-
+	get(State, [X,Y], Enemy),
+	Enemy \= Plyr,
+	Enemy \= .,
+	incXDecY(X,Y, NewX, NewY),
+	countEnemiesNE(Plyr, [NewX, NewY], State, ValidDot).
+countEnemiesNE(_, [X,Y], _, ValidDot):-
+	ValidDot = [[X, Y]].
+
+countEnemiesNW(Plyr, [X,Y], State, ValidDot) :-
+	get(State, [X,Y], Enemy),
+	Enemy \= Plyr,
+	Enemy \= .,
+	decXIncY(X,Y, NewX, NewY),
+	countEnemiesNW(Plyr, [NewX, NewY], State, ValidDot).
+countEnemiesNW(_, [X,Y], _, ValidDot):-
+	ValidDot = [[X, Y]].
+
+west(_, State, [], []) :-
+	showState(State).
+west(Plyr, State, [[X, Y]|PlayerCoordinates], DotPosition) :-
+	NextX is X-1,
+	get(State, [NextX,Y], Enemy),
+	Enemy \= Plyr,
+	Enemy \= .,
+	NextNextX is NextX - 1,
+	countEnemiesWest(Plyr, [NextNextX, Y], State, PlayablePosition),
+	west(Plyr, State, PlayerCoordinates, NewDot), !,
+	append(PlayablePosition, NewDot, DotPosition).
+west(Plyr, State, [_|PlayerCoordinates], DotPosition) :-
+	west(Plyr, State, PlayerCoordinates, DotPosition).
+
+east(_,_,[],[]).
+east(Plyr, State, [[X, Y]| PlayerCoordinates], DotPosition) :-
+	increaseX(X, NextX),
+	get(State, [NextX, Y], Enemy),
+	Enemy \= Plyr,
+	Enemy \= .,
+	increaseX(NextX, NextNextX),
+	countEnemiesEast(Plyr, [NextNextX, Y], State, PlayablePosition),
+	east(Plyr, State, PlayerCoordinates, NewDot), !,
+	append(PlayablePosition, NewDot, DotPosition).
+east(Plyr, State, [_|PlayerCoordinates], DotPosition) :-
+	east(Plyr, State, PlayerCoordinates, DotPosition).
+
+north(_,State,[],[]) :-
+	showState(State).
+north(Plyr, State, [[X,Y] | PlayerCoordinates], DotPosition) :-
+	decreaseY(Y, NextY),
+	get(State, [X, NextY], Enemy),
+	Enemy \= Plyr,
+	Enemy \= .,
+	decreaseY(NextY, NextNextY),
+	countEnemiesNorth(Plyr, [X, NextNextY], State, PlayablePosition),
+	north(Plyr, State, PlayerCoordinates, NewDot), !,
+	append(PlayablePosition, NewDot, DotPosition).
+north(Plyr, State, [_|PlayerCoordinates], DotPosition) :-
+	north(Plyr, State, PlayerCoordinates, DotPosition).
+
+south(_, State, [], []) :-
+	showState(State).
+south(Plyr, State, [[X,Y] | PlayerCoordinates], DotPosition) :-
+	increaseY(Y, NextY),
+	get(State, [X, NextY], Enemy),
+	Enemy \= Plyr,
+	Enemy \= .,
+	increaseY(NextY, NextNextY),
+	countEnemiesSouth(Plyr, [X, NextNextY], State, PlayablePosition),
+	south(Plyr, State, PlayerCoordinates, NewDot), !,
+	append(PlayablePosition, NewDot, DotPosition).
+south(Plyr, State, [_|PlayerCoordinates], DotPosition) :-
+	south(Plyr, State, PlayerCoordinates, DotPosition).
+
+southEast(_, State, [], []) :-
+	showState(State).
+southEast(Plyr, State, [[X,Y] | PlayerCoordinates], DotPosition) :-
+	increaseXY(X, Y, NextX, NextY),
+	get(State, [NextX, NextY], Enemy),
+	Enemy \= Plyr,
+	Enemy \= .,
+	increaseXY(NextX, NextY, NextNextX, NextNextY),
+	countEnemiesSE(Plyr, [NextNextX, NextNextY], State, PlayablePosition),
+	southEast(Plyr, State, PlayerCoordinates, NewDot), !,
+	append(PlayablePosition, NewDot, DotPosition).
+southEast(Plyr, State, [_|PlayerCoordinates], DotPosition) :-
+	southEast(Plyr, State, PlayerCoordinates, DotPosition).
+
+southWest(_, State, [], []) :-
+	showState(State).
+southWest(Plyr, State, [[X,Y] | PlayerCoordinates], DotPosition) :-
+	decreaseXY(X, Y, NextX, NextY),
+	get(State, [NextX, NextY], Enemy),
+	Enemy \= Plyr,
+	Enemy \= .,
+	decreaseXY(NextX, NextY, NextNextX, NextNextY),
+	countEnemiesSW(Plyr, [NextNextX, NextNextY], State, PlayablePosition),
+	southWest(Plyr, State, PlayerCoordinates, NewDot), !,
+	append(PlayablePosition, NewDot, DotPosition).
+southWest(Plyr, State, [_|PlayerCoordinates], DotPosition) :-
+	southWest(Plyr, State, PlayerCoordinates, DotPosition).
+
+northEast(_, State, [], []) :-
+	showState(State).
+northEast(Plyr, State, [[X,Y] | PlayerCoordinates], DotPosition) :-
+	incXDecY(X, Y, NextX, NextY),
+	get(State, [NextX, NextY], Enemy),
+	Enemy \= Plyr,
+	Enemy \= .,
+	incXDecY(NextX, NextY, NextNextX, NextNextY),
+	countEnemiesNE(Plyr, [NextNextX, NextNextY], State, PlayablePosition),
+	northEast(Plyr, State, PlayerCoordinates, NewDot), !,
+	append(PlayablePosition, NewDot, DotPosition).
+northEast(Plyr, State, [_|PlayerCoordinates], DotPosition) :-
+	northEast(Plyr, State, PlayerCoordinates, DotPosition).
+
+northWest(_, State, [], []) :-
+	showState(State).
+northWest(Plyr, State, [[X,Y] | PlayerCoordinates], DotPosition) :-
+	decXIncY(X, Y, NextX, NextY),
+	get(State, [NextX, NextY], Enemy),
+	Enemy \= Plyr,
+	Enemy \= .,
+	decXIncY(NextX, NextY, NextNextX, NextNextY),
+	countEnemiesNW(Plyr, [NextNextX, NextNextY], State, PlayablePosition),
+	northWest(Plyr, State, PlayerCoordinates, NewDot), !,
+	append(PlayablePosition, NewDot, DotPosition).
+northWest(Plyr, State, [_|PlayerCoordinates], DotPosition) :-
+	northWest(Plyr, State, PlayerCoordinates, DotPosition).	
+
+moves(Plyr, State, MvLst) :-
+	getAllPlayerPositions(Plyr, State, 0, PlyrPositions),
+	west(Plyr, State, PlyrPositions, ValidWest) ,
+	east(Plyr, State, PlyrPositions, ValidEast) ,
+	south(Plyr, State, PlyrPositions, ValidSouth) ,
+	north(Plyr, State, PlyrPositions, ValidNorth) ,
+	southEast(Plyr, State, PlyrPositions, ValidSE) ,
+	southWest(Plyr,State, PlyrPositions, ValidSW) ,
+	northEast(Plyr, State, PlyrPositions, ValidNE) ,
+	northWest(Plyr, State, PlyrPositions, ValidNW) ,
+	append(ValidWest, ValidEast, ValidWestEast),
+	append(ValidWestEast, ValidSouth, ValidWES),
+	append(ValidWES, ValidNorth, ValidWESN),
+	append(ValidWESN, ValidSE, ValidWESNSE),
+	append(ValidWESNSE, ValidSW, ValidWESNSESW),
+	append(ValidWESNSESW, ValidNE, ValidWESNSESWNE),
+	append(ValidWESNSESWNE, ValidNW, MvLst).
 
 % DO NOT CHANGE THIS BLOCK OF COMMENTS.
 %
